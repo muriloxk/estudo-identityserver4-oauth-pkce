6af3d035b8d8150d34c3b7fc855fa6cf8d65e14d
Murilo Sanches <murilando@gmail.com>
2020-03-25 23:42:00 -0300

    first commit

---
+++ Starter/src/Murilo.IDP/Config.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Murilo.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[] 
+            { };
+        
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { 
+                new Client{
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RedirectUris = new List<string>()
+                    {
+                        "http://localhost:44389/signin-oidc"
+                    },
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.OpenId, 
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets = {
+                        new Secret("Secret".Sha256())
+                    }
+                }
+            };
+        
+    }
+}
